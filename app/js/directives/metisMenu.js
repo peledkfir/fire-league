72b989d16a100c4ee51f5d3b3313a2ee07058a23
Kfir Peled <kfir.peled@jivesoftware.com>
2014-02-15 15:08:55 +0200

    relayout navbars

--- app/js/directives/metisMenu.js
+++ app/js/directives/metisMenu.js
@@ -3,8 +3,23 @@
 fApp.directive('metisMenu', function() {
 	return {
 		restrict: 'A',
-		link: function(scope, element) {
+		link: function(scope, element, attrs) {
 			element.metisMenu();
+		}
+	};
+})
+.directive('metisMenuCollapse', function() {
+	return {
+		restrict: 'A',
+		link: function(scope, element, attrs) {
+
+			$(window).bind("load resize", function() {
+		        if ($(this).width() < attrs.metisMenuCollapse) {
+		            element.addClass('collapse');
+		        } else {
+		            element.removeClass('collapse');
+		        }
+		    });
 		}
 	};
 });
