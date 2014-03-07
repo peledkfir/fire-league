5f9f2ee926f341bee2b86bb0701e8efc94d9cda7
Kfir Peled <kfir.peled@jivesoftware.com>
2014-03-08 00:31:01 +0200

    delete config

--- app/js/config.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('fire-league.config', [])
-	.config(['FacebookProvider', function(FacebookProvider) {
-	     // Here you could set your appId throug the setAppId method and then initialize
-	     // or use the shortcut in the initialize method directly.
-	     FacebookProvider.init('622249227842681');
-	}])
-	.constant('FBURL', 'https://fireleague.firebaseio.com');
